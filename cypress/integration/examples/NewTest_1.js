e72a4823338d87adbe22362a5ee6926349f87d8b
unknown <chinnuteju09@gmail.com>
2020-04-12 22:17:45 +0530

    Hello

---
+++ cypress/integration/examples/NewTest_1.js
@@ -0,0 +1,35 @@
+describe('Second Test Suite', function()
+{
+   it('First Test Case', function()
+   
+   {
+
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+
+   }
+
+   ) 
+
+   it('Second Test Case', function()
+   
+   {
+
+    cy.visit('https://www.google.com')
+
+   }
+
+   ) 
+
+   it('Third Test Case', function()
+   
+   {
+
+    cy.visit('https://www.google.com')
+
+   }
+
+   ) 
+ 
+}
+
+)
